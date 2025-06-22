7f5c2e988ab432c7040458a16a2da43b620ed98f
dxhdong <72720243+dxhdong@users.noreply.github.com>
2025-06-22 23:49:34 +0800

    formatter
    formatter

--- index.js
+++ index.js
@@ -6,58 +6,61 @@
 exports.example = () => 'hello world';
 
 exports.stripPrivateProperties = (props, objs) => {
-    return objs.map(obj => {
-      const newObj = { ...obj }; 
-      props.forEach(prop => {
-        delete newObj[prop]; 
-      });
-      return newObj;
+  return objs.map((obj) => {
+    const newObj = { ...obj };
+    props.forEach((prop) => {
+      delete newObj[prop];
     });
-  };
+    return newObj;
+  });
+};
 exports.excludeByProperty = (prop, objs) => {
-  return objs.filter(obj => !obj.hasOwnProperty(prop));
+  return objs.filter((obj) => !obj.hasOwnProperty(prop));
 };
 exports.sumDeep = (items) => {
-    return items.map(item => {
-      const sum = item.objects.reduce((total, obj) => total + obj.val, 0);
-      return { objects: sum };
-    });
-  };
+  return items.map((item) => {
+    const sum = item.objects.reduce((total, obj) => total + obj.val, 0);
+    return { objects: sum };
+  });
+};
 exports.applyStatusColor = (colorMap, statuses) => {
-    const statusToColor = Object.entries(colorMap).reduce((acc, [color, codes]) => {
-      codes.forEach(code => {
+  const statusToColor = Object.entries(colorMap).reduce(
+    (acc, [color, codes]) => {
+      codes.forEach((code) => {
         acc[code] = color;
       });
       return acc;
-    }, {});
-  
-    return statuses
-      .filter(item => statusToColor[item.status] !== undefined)
-      .map(item => ({
-        ...item,
-        color: statusToColor[item.status],
-      }));
+    },
+    {}
+  );
+
+  return statuses
+    .filter((item) => statusToColor[item.status] !== undefined)
+    .map((item) => ({
+      ...item,
+      color: statusToColor[item.status],
+    }));
 };
 exports.createGreeting = (greetFn, greeting) => {
-    return (name) => greetFn(greeting, name);
+  return (name) => greetFn(greeting, name);
 };
 exports.setDefaults = (defaultProps) => {
-    return (obj) => {
-      return { ...defaultProps, ...obj };
-    };
+  return (obj) => {
+    return { ...defaultProps, ...obj };
+  };
 };
 exports.fetchUserByNameAndUsersCompany = async (name, services) => {
-    const users = await services.fetchUsers();
-    const user = users.find(u => u.name === name);
-  
-    const [company, status] = await Promise.all([
-      services.fetchCompanyById(user.companyId),
-      services.fetchStatus(),
-    ]);
-  
-    return {
-      user,
-      company,
-      status,
-    };
+  const users = await services.fetchUsers();
+  const user = users.find((u) => u.name === name);
+
+  const [company, status] = await Promise.all([
+    services.fetchCompanyById(user.companyId),
+    services.fetchStatus(),
+  ]);
+
+  return {
+    user,
+    company,
+    status,
   };
+};
